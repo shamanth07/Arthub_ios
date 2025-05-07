2d4b0c77c85a9e2f632855d55d32e10b9ca7c142
shamanth07 <shamanthanireddy07@gmail.com>
2025-05-06 21:50:06 -0400

    Initial Commit

---
+++ ArtHub/ArtHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtHubApp.swift
+//  ArtHub
+//
+//  Created by User on 2025-04-27.
+//
+
+import SwiftUI
+
+@main
+struct ArtHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArtHubTests/ArtHubTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArtHubTests.swift
+//  ArtHubTests
+//
+//  Created by User on 2025-04-27.
+//
+
+import Testing
+@testable import ArtHub
+
+struct ArtHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
