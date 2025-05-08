9c75593afa4023b315d89381b18e0432bed5aa8e
shamanth07 <shamanthanireddy07@gmail.com>
2025-05-08 11:10:10 -0400

    Second ccommit

---
+++ ArtHubProject/ArtHubProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtHubProjectApp.swift
+//  ArtHubProject
+//
+//  Created by User on 2025-05-08.
+//
+
+import SwiftUI
+
+@main
+struct ArtHubProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArtHubProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ArtHubProject
+//
+//  Created by User on 2025-05-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
