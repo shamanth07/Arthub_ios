2b84097bcb5363b40c562951dec3fbc810530b51
shamanth07 <shamanthanireddy07@gmail.com>
2025-06-05 11:52:59 -0400

    Initial Commit

---
+++ ArtHub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ArtHub
+//
+//  Created by User on 2025-06-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
